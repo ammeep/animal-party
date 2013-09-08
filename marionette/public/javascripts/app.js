c4ec2162541a8ae24dbab8796424ba7c7b918ac6
Amy Palamountain <amy@palamounta.in>
2013-09-08 21:31:19 +1200

    refactored the app router into a view manager

--- marionette/public/javascripts/app.js
+++ marionette/public/javascripts/app.js
@@ -4,30 +4,15 @@ PartyApp = (function(Backbone, Marionette){
   var App = new Marionette.Application();
 
   App.addRegions({
-    main: "#app-container" 
+    mainRegion: "#app-container" 
   });
 
   App.on("initialize:after", function(){
-  	App.Router = new PartyApp.Router();
     if (Backbone.history){
       Backbone.history.start({pushState: true});
     }	
   });
 
-  App.showApp = function(appName, args){
-    var currentApp = App.module(appName);
-    if (App.currentApp === currentApp){ return; }
-
-    if (App.currentApp){
-      App.currentApp.stop();
-    }
-
-    App.currentApp = currentApp;
-    currentApp.start(args);
-    currentApp.show();
-    return currentApp;
-  };
- 
 	$(function () {
 		 App.start();	
 		 //var app = App.showApp('GuestList',{region:App.main});
--- marionette/public/javascripts/routers/party.router.js
+++ marionette/public/javascripts/routers/party.router.js
@@ -1,22 +1,48 @@
-/*global Backbone */
-PartyApp.Router = (function(App, Backbone){
-  "use strict";
+PartyApp.module("ViewManager", function(ViewManager, App, Backbone, Marionette, $, _){
+  	
+  	var Controller, Router, router, controller;
 
-    var Router = Backbone.Router.extend({
+	Router = Marionette.AppRouter.extend({
+	  appRoutes: {
+   		'': 'showGuestList',
+		'start-the-party': 'startTheParty'
+	  },
+	});
 
-     	routes: {
-			'': 'showGuestList',
-			'start-the-party': 'startTheParty'
-		},
+  	Controller = Marionette.Controller.extend({
 
-		showGuestList: function () {
-			PartyApp.showApp('GuestList',{region:PartyApp.main});
-		},
-		startTheParty: function () {
-			PartyApp.showApp('PartyAnimals',{region:PartyApp.main});
+  		showGuestList: function(){
+  			this.startModule('GuestList',{region:App.mainRegion})
+  		},
+
+  		startTheParty: function(){
+  			this.startModule('PartyAnimals',{region:App.mainRegion})
+  		},
+
+  		startModule: function(moduleName, args){
+		    var currentModule = App.module(moduleName);
+		    if (this.currentModule === currentModule){ return; }
+
+		    if (this.currentModule){
+		      this.currentModule.stop();
+		    }
+
+		    this.currentModule = currentModule;
+		    currentModule.start(args);
+		    currentModule.show();
+		    return currentModule;
 		}
+  	});
 
-    });
-  return Router;
 
-})(PartyApp, Backbone); 
+  	ViewManager.on("before:start", function(options){
+  		controller = new Controller();
+    	router = new Router({controller: controller});
+  	});
+  	
+  	ViewManager.on("before:stop",function(){
+    	if (router){
+      		delete router;
+    	}
+ 	});
+});
